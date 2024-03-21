c985d88fa185f2df53f61fbabec30f671ab7bee9
diegolopez-dev <diego.teide@gmail.com>
2024-03-21 02:39:41 +0100

    Modificando buid

--- build.sh
+++ build.sh
@@ -10,7 +10,4 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python world_champ_2022/manage.py createsuperuser --no-input
-fi
+python manage.py createsuperuser --no-input
