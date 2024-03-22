6dec0821d8402924630a36ba2e1ef0750667e1e4
diegolopez-dev <diego.teide@gmail.com>
2024-03-22 01:59:17 +0100

    Modificando tienda

--- mariarte/settings.py
+++ mariarte/settings.py
@@ -138,7 +138,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = os.path.dirname('media')
 
